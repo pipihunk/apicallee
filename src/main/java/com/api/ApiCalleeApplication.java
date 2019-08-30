f5f9f22a4e2b13c11d27608fff812b50a4901707
Bo Zhang <pipihunk@gmail.com>
2019-08-30 09:27:33 -0400

    first

---
+++ src/main/java/com/api/ApiCalleeApplication.java
@@ -0,0 +1,13 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCalleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCalleeApplication.class, args);
+	}
+
+}
